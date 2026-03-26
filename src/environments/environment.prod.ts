94b9847d092f3d1d27953d6a21342df657ee1112
Venkat-PC <venkat.m@indowestunited.com>
2026-03-26 16:28:28 +0530

    Changes done.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api'
+  apiUrl: 'https://smartevaluate-app-bjapanf4h7h7hsgy.centralindia-01.azurewebsites.net/api'
 };
