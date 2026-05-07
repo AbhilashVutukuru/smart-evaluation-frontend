c9c76a0d49a3ac2c5632380eb4dd4e049ae44580
Venkat Mandla <venkat.m@indowestunited.com>
2026-05-07 21:08:08 +0530

    Changes done.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://smartevaluate-app-bjapanf4h7h7hsgy.centralindia-01.azurewebsites.net/api',
-  apiUrl: 'https://api.ai-smartgrader.com/api',
+  apiUrl: 'https://smartevaluate-app-bjapanf4h7h7hsgy.centralindia-01.azurewebsites.net/api',
+  //apiUrl: 'https://api.ai-smartgrader.com/api',
 };
